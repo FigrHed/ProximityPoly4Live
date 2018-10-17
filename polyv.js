f39aa4c78f79da1cfba8e01caaf453a36dd745f6
Tyson <tyson@192-168-1-101.tpgi.com.au>
2018-10-17 22:00:11 +1100

    initial commit

--- polyv.js
+++ polyv.js
@@ -1,26 +1,79 @@
-inlets = 1;
-outlets = 3;
 
-function voice(pitch,velocity){
-	this.pitch = pitch;
-	this.velocity = velocity;
-}
-
-//var voice1 = new voice(0,0);
-//var voice2 = new voice(0,0);
-//var voice3 = new voice(0,0);
-
-
-//var voices1 = new Array();
-//var voices2 = new Array();
-//var voices3 = new Array();
-var voices = [];
-//var voices = [voices1,voices2,voices3];
 var numVoices = 0;
 var limit = 3;
+var voices;
+init();
+
+function voice(pitch,velocity) {
+	this.pitch = pitch;
+	this.velocity = velocity;
+    this.printOut = function() {
+        return "pitch: " + this.pitch + " Velocity: " + this.velocity;
+    }
+}
+
+
+function init(){
+    inlets = 1;
+    outlets = limit;
+    numVoices = 0;
+    //initiate voice array
+    voices = [];
+    for(i=0;i<limit;i++){
+        voices.push(new Array());
+    }
+}
+
+//Testing
+//post();
+//post(voices[0].print());
+
+
+//reset all voices
+function panic(){
+    for(i=0;i<limit;i++){
+        outlet(i,voices[i].pitch,voices[i].velocity,i+1);
+    }
+
+    init();
+    
+    numVoices = 0;
+}
+
+function assign(voiCe){
+			var min;
+			var closestVoice = 0;
+			var distances = [];
+			var dist;
+			min = 100;
+
+
+			for(i=0;i<limit;i++){
+				dist = Math.abs(voices[i][voices[i].length-1].pitch - voiCe.pitch);
+//				post("dist: " + dist);
+//				post();
+				distances.push(dist);
+				
+				if(distances[i] < min){
+					min = dist;
+					closestVoice = i;
+					
+				}
+			}
+//			post("Closest Voice: " + closestVoice);
+//			post();
+//			post("Minimum: " + min);
+//			post();
+			min = Math.min(distances);
+			voices[closestVoice].push(voiCe);
+			outlet(closestVoice,voiCe.pitch,voiCe.velocity);
+//			post("Turned on:",voiCe.pitch,"at",1);
+//			post();
+}
 
 function msg_int(maxVoices){
 	limit = maxVoices;
+    init();
 }
 
 function list(){
@@ -28,57 +81,42 @@ function list(){
 	var pitch = a[0];
 	var velocity = a[1];
 	var voiCe = new voice(pitch,velocity);
-	//post("Current Pitch:",voiCe.pitch,"Current Velocity:",voiCe.velocity);
-	//post();
+//	post("Current Pitch:",voiCe.pitch,"Current Velocity:",voiCe.velocity);
+//	post();
 	if(velocity > 0){
 		numVoices++;
-		for(i=0;i<3;i++){
+
+        //Add to earliest avaible spot in array.
+		for(i=0;i<limit;i++){
 			if (voices[i].length == 0){
 				voices[i].push(voiCe);
 				outlet(i,voiCe.pitch,voiCe.velocity,i+1);
 				
-				
 				break;
 			}
 		}
-		if(numVoices > 3){
+        //if all spots are taken
+		if(numVoices > limit){
 			// assign based on proximity
-			var min;
-			var dist1 = Math.abs(voices[0][voices[0].length-1].pitch - voiCe.pitch);
-			var dist2 = Math.abs(voices[1][voices[1].length-1].pitch - voiCe.pitch);
-			var dist3 = Math.abs(voices[2][voices[2].length-1].pitch - voiCe.pitch);
-			if(dist1 < dist2 && dist1 < dist2){
-				min = dist1;
-				voices[0].push(voiCe);
-				outlet(0,voiCe.pitch,voiCe.velocity,1);
-				//post("Turned on:",voiCe.pitch,"at",1);
-				//post();
-			} else if(dist2 < dist3){
-				voices[1].push(voiCe);
-				outlet(1,voiCe.pitch,voiCe.velocity,2);
-				//post("Turned on:",voiCe.pitch,"at",2);
-				//post();
-			} else {
-				voices[2].push(voiCe);
-				outlet(2,voiCe.pitch,voiCe.velocity,3);
-				//post("Turned on:",voiCe.pitch,"at",3);
-				//post();
-			}
-		}		
+//input: voiCe
+			assign(voiCe);
+			
+		}	
 	} else {
-		numVoices--;
-		for(i=0;i<3;i++){
+		//If note-off, remove from array
+		for(i=0;i<limit;i++){
 			for(j=0;j<voices[i].length;j++){
 				if(voices[i][j].pitch == voiCe.pitch){
 					outlet(i,voiCe.pitch,0,i+1);
 					voices[i].splice(j,1);
-					
+					numVoices--;
 					
 					//post("Turned Off voice",i+1);
 					//post();
 				}
 			}
 		}
+        
 	}
 //	for(i=0;i<3;i++){
 //		post("Voices ",i+1,"length:",voices[i].length);
@@ -87,6 +125,6 @@ function list(){
 //		}
 //		post();
 //	}
-	//post("Number of Voices:",numVoices);
-	//post();
+	post("Number of Voices:",numVoices);
+	post();
 }
